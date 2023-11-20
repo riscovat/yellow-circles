f8d2886d5822b36cd8c89e50533e436769ad2af6
Arina <riscovat@yandex.ru>
2023-11-20 20:49:55 +0300

    HI

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QPen, QColor, QBrush
+from PyQt5.QtCore import Qt
+from random import randint
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class GoodMoodRising(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.color = QColor(255, 255, 0)
+        self.draw = False
+
+    def run(self):
+        self.draw = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.draw:
+            qp = QPainter(self)
+            qp.begin(self)
+            pen = QBrush(self.color)
+            size = randint(0, 400)
+            qp.setBrush(pen)
+            qp.drawEllipse(150, 50, size, size)
+            qp.end()
+            self.draw = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = GoodMoodRising()
+    ex.show()
+    sys.exit(app.exec_())
